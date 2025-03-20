9c645b1b68aab2b73ca7e7391f4563b255628946
Luan Cordeiro <97892542+Luanlrc@users.noreply.github.com>
2025-03-20 20:46:33 -0300

    Update jogo_velha.c

--- 2025/projeto_integrador/jogo_velha.c
+++ 2025/projeto_integrador/jogo_velha.c
@@ -2,48 +2,58 @@
 #include <string.h>
 #include <stdlib.h>
 
+	// setlocale(LC_ALL, "Portuguese")
 	// int aux;
 	// for(aux = 1; aux <= 20; aux++){
 	//     printf(" \n");
 	//   };
+	// 88 = X
+	// 117 = O
+	// 240 = caracter invisivel
+	// tabela ascii https://www.matematica.pt/util/resumos/tabela-ascii.php
 	
 char player_one[20];
 char player_two[20];
 
 void template_line(int aux){
-	for(aux = 1; aux <= 6; aux++){
+	int auxi;
+	for(auxi = 1; auxi <= aux; auxi++){
 		printf("                          ");
 	};
 }
 
-void template_break_line(aux){
-	for(aux = 1; aux <= 6; aux++){
+void template_break_line(int aux){
+	int auxi;
+	for(auxi = 1; auxi <= aux; auxi++){
 		printf("                          \n");
 	};
 }
 
 
-void game_table(void){
+void game_table(int pri){
 	printf("                          ***************\n");
 	printf("                          *    |   |    *\n");
+	printf("                          *  %c |   |    *\n", pri);
 	printf("                          *____|___|____*\n");
 	printf("                          *    |   |    *\n");
+	printf("                          *    |   |    *\n");
 	printf("                          *____|___|____*\n");
+	printf("                          *    |   |    *\n");
 	printf("                          *    |   |    *\n");
 	printf("                          *    |   |    *\n");
 	printf("                          ***************\n");
 }
 
-void table_template(void){
-	template_break_line(6);
-	game_table();
+void table_template(int pri){
 	template_break_line(3);
+	game_table(pri);
+	template_break_line(2);
 }
 
 
 void start_game(void){
 	system("cls");
-	table_template();
+	table_template(88);
 	printf("                     Digite o nome do player 1:\n");
 	printf("                               ");
 	scanf("%49s", &player_one);
@@ -53,7 +63,7 @@ void start_game(void){
 
 void menu_principal(char player_one[], char player_two[]){
 	system("cls");
-	table_template();
+	table_template(88);
 	printf("                                Menu                               \n");
 	printf("                                                                   \n");
 	printf("                            1 - Jogo Padrao                        \n");
