76466e6f60f74ca7f28bfaa6e338358020a77721
Luan Cordeiro <97892542+Luanlrc@users.noreply.github.com>
2025-03-22 09:59:44 -0300

    Update jogo_velha.c

--- 2025/projeto_integrador/jogo_velha.c
+++ 2025/projeto_integrador/jogo_velha.c
@@ -15,6 +15,37 @@
 char player_one[20];
 char player_two[20];
 
+char verify_winner(char tabuleiro[SIZE][SIZE]) {
+    // Verifica linhas e colunas
+    for (int i = 0; i < SIZE; i++) {
+        if (tabuleiro[i][0] != ' ' && tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2]) {
+            return tabuleiro[i][0];
+        }
+        if (tabuleiro[0][i] != ' ' && tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i]) {
+            return tabuleiro[0][i];
+        }
+    }
+
+    // Verifica diagonais
+    if (tabuleiro[0][0] != ' ' && tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) {
+        return tabuleiro[0][0];
+    }
+    if (tabuleiro[0][2] != ' ' && tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]) {
+        return tabuleiro[0][2];
+    }
+
+    // Verifica se há espaços vazios para continuar o jogo
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (tabuleiro[i][j] == ' ') {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
 void template_line(int aux){
 	int auxi;
 	for(auxi = 1; auxi <= aux; auxi++){
@@ -84,5 +115,20 @@ void menu_principal(char player_one[], char player_two[]){
 
 int main() {
 	start_game();
+	char tabuleiro[SIZE][SIZE] = {
+		{'X', 'O', 'X'},
+		{'O', 'X', 'O'},
+		{'O', 'X', 'X'}
+	};
+	
+	char resultado = verify_winner(tabuleiro);
+	
+	if (resultado == 'X' || resultado == 'O') {
+		printf("O jogador '%c' venceu!\n", resultado);
+	} else if (resultado == 'E') {
+		printf("O jogo terminou em empate!\n");
+	} else {
+		printf("O jogo ainda não terminou.\n");
+	}
     return 0;
 }
