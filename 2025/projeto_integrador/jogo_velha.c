8c38c8273f0316078282a5fff38e9a872820adfe
Luan Cordeiro <97892542+Luanlrc@users.noreply.github.com>
2025-04-10 20:49:45 -0300

    Update jogo_velha.c

--- 2025/projeto_integrador/jogo_velha.c
+++ 2025/projeto_integrador/jogo_velha.c
@@ -1,21 +1,76 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <windows.h>
 #define SIZE 3
+#define WIDTH 50
 	// setlocale(LC_ALL, "Portuguese")
 	// int aux;
 	// for(aux = 1; aux <= 20; aux++){
 	//     printf(" \n");
 	//   };
+	
+	// tabela ascii https://www.matematica.pt/util/resumos/tabela-ascii.php
+	// gotoxy(10,5) colunas / linhas
+	
+	//201	canto superior esquerdo
+	//200 canto inferior esquerdo
+	//187	superior direito
+	//188 inferior direito
+	//205 horizontal
+	//186 vertical
 	// 88 = X
 	// 117 = O
 	// 240 = caracter invisivel
-	// tabela ascii https://www.matematica.pt/util/resumos/tabela-ascii.php
-	// gotoxy(10,5) colunas / linhas
 	
 char player_one[20];
 char player_two[20];
 
+void render_line_up(){ // renderiza a linha de cima
+	int aux, aux2;
+	printf("%c", 201);
+	for(aux = 1; aux <= WIDTH; aux++){
+		printf("%c", 205);
+	}
+	printf("%c\n", 187);
+}
+
+void render_line_down(){ // renderiza a linha de baixo
+	int aux, aux2;
+	printf("%c", 200);
+	for(aux = 1; aux <= WIDTH; aux++){
+		printf("%c", 205);
+	}
+	printf("%c\n", 188);
+}
+
+void render_body(){
+	int aux, aux2;
+	for(aux2 = 1; aux2 <= 13; aux2++){
+		printf("%c", 186);
+		for(aux = 1; aux <= WIDTH; aux++){
+			printf(" ");
+		}
+		printf("%c\n", 186);
+	}
+}
+
+//Função gotoxy
+void gotoxy(int x, int y){
+  COORD coord;
+  coord.X = x;
+  coord.Y = y;
+  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void template_test(){
+	render_line_up();
+	render_body();
+	render_line_down();
+	gotoxy(10,1); //colunas / linhas
+	table_template(88);
+	getchar();
+}
 
 char verify_winner(char tabuleiro[SIZE][SIZE]) {
 	int i,j;
@@ -52,36 +107,38 @@ char verify_winner(char tabuleiro[SIZE][SIZE]) {
 void template_line(int aux){
 	int auxi;
 	for(auxi = 1; auxi <= aux; auxi++){
-		printf("                          ");
+		printf("%c                          ",186);
 	};
 }
 
 void template_break_line(int aux){
 	int auxi;
 	for(auxi = 1; auxi <= aux; auxi++){
-		printf("                          \n");
+		printf("%c                          \n",186);
 	};
 }
 
 
 void game_table(int pri){
-	printf("                          ***************\n");
-	printf("                          *    |   |    *\n");
-	printf("                          *  %c |   |    *\n", pri);
-	printf("                          *____|___|____*\n");
-	printf("                          *    |   |    *\n");
-	printf("                          *    |   |    *\n");
-	printf("                          *____|___|____*\n");
-	printf("                          *    |   |    *\n");
-	printf("                          *    |   |    *\n");
-	printf("                          *    |   |    *\n");
-	printf("                          ***************\n");
+	printf("***************\n");
+	printf("*    |   |    *\n");
+	printf("*  %c |   |    *\n",pri);
+	printf("*____|___|____*\n");
+	printf("*    |   |    *\n");
+	printf("*    |   |    *\n");
+	printf("*____|___|____*\n");
+	printf("*    |   |    *\n");
+	printf("*    |   |    *\n");
+	printf("*    |   |    *\n");
+	printf("***************\n");
 }
 
 void table_template(int pri){
-	template_break_line(3);
+	//template_break_line(1);
+	printf("\n");
 	game_table(pri);
-	template_break_line(2);
+	printf("\n");
+	//template_break_line(1);
 }
 
 
@@ -117,6 +174,7 @@ void menu_principal(char player_one[], char player_two[]){
 
 
 int main() {
+	template_test();
 	start_game();
 	char tabuleiro[SIZE][SIZE] = {
 		{'X', 'O', 'X'},
